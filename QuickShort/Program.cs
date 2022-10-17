3a190f3932c9d1c772ebc7cba847850e8b8d1aee
Wulan <ASUS@LAPTOP-781R3HVH>
2022-10-17 17:30:42 +0700

    array of integers to hold values

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -8,9 +8,89 @@ namespace QuickShort
 {
     internal class Program
     {
-        
-        static void Main(string[] args)
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // number of elements in array 
+        private int n;
+
+
+        void read()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("\n==================");
+
+            //get array elements
+            for (int i = 0;i < n;i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+          }
+        //swaps the element at index x with the element at index y 
+        
+        void swap (int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp; 
+
+        }
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+
+            //Partition the list into two parts:
+            //one containing elements less that or equal to pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //Search for an element greather than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //Search for an element less than or equal to pivot
+                while ((arr[j] <= pivot) && (i <= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) //if the greather element is on the left of the element 
+                {
+                    //swap the element at index i whit the element at index j
+                    swap(i, j);
+                }
+            }
         }
     }
 }
